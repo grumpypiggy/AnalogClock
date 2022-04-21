bfbf0c6b92212e580aab4cb3bdd14a9decca5468
grumpypiggy <95675166+grumpypiggy@users.noreply.github.com>
2022-04-21 11:26:02 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace AnalogClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupTimeDisplay = new System.Windows.Forms.GroupBox();
+            this.lblStopWatch = new System.Windows.Forms.Label();
+            this.groupStopWatch = new System.Windows.Forms.GroupBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.btnRecord = new System.Windows.Forms.Button();
+            this.groupMode = new System.Windows.Forms.GroupBox();
+            this.btnAnalogClock = new System.Windows.Forms.RadioButton();
+            this.btnStopWatch = new System.Windows.Forms.RadioButton();
+            this.timerClock = new System.Windows.Forms.Timer(this.components);
+            this.timerStopWatch = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.groupTimeDisplay.SuspendLayout();
+            this.groupStopWatch.SuspendLayout();
+            this.groupMode.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.groupTimeDisplay);
+            this.panel1.Controls.Add(this.groupStopWatch);
+            this.panel1.Controls.Add(this.groupMode);
+            this.panel1.Location = new System.Drawing.Point(359, 449);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(476, 402);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DimGray;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(451, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "X";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // groupTimeDisplay
+            // 
+            this.groupTimeDisplay.Controls.Add(this.lblStopWatch);
+            this.groupTimeDisplay.Location = new System.Drawing.Point(12, 24);
+            this.groupTimeDisplay.Name = "groupTimeDisplay";
+            this.groupTimeDisplay.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.groupTimeDisplay.Size = new System.Drawing.Size(441, 74);
+            this.groupTimeDisplay.TabIndex = 3;
+            this.groupTimeDisplay.TabStop = false;
+            this.groupTimeDisplay.Text = "Time";
+            // 
+            // lblStopWatch
+            // 
+            this.lblStopWatch.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblStopWatch.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStopWatch.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblStopWatch.Location = new System.Drawing.Point(3, 22);
+            this.lblStopWatch.Name = "lblStopWatch";
+            this.lblStopWatch.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblStopWatch.Size = new System.Drawing.Size(435, 49);
+            this.lblStopWatch.TabIndex = 1;
+            this.lblStopWatch.Text = "Time";
+            this.lblStopWatch.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupStopWatch
+            // 
+            this.groupStopWatch.Controls.Add(this.btnStop);
+            this.groupStopWatch.Controls.Add(this.btnStart);
+            this.groupStopWatch.Controls.Add(this.btnClear);
+            this.groupStopWatch.Controls.Add(this.listBox);
+            this.groupStopWatch.Controls.Add(this.btnRecord);
+            this.groupStopWatch.Enabled = false;
+            this.groupStopWatch.Location = new System.Drawing.Point(12, 177);
+            this.groupStopWatch.Name = "groupStopWatch";
+            this.groupStopWatch.Size = new System.Drawing.Size(441, 210);
+            this.groupStopWatch.TabIndex = 2;
+            this.groupStopWatch.TabStop = false;
+            this.groupStopWatch.Text = "Stopwatch  Settings";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(10, 76);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(119, 31);
+            this.btnStop.TabIndex = 6;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(10, 39);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(225, 31);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(135, 76);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(100, 31);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // listBox
+            // 
+            this.listBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 20;
+            this.listBox.Location = new System.Drawing.Point(10, 113);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(425, 84);
+            this.listBox.TabIndex = 4;
+            // 
+            // btnRecord
+            // 
+            this.btnRecord.Location = new System.Drawing.Point(241, 39);
+            this.btnRecord.Name = "btnRecord";
+            this.btnRecord.Size = new System.Drawing.Size(194, 68);
+            this.btnRecord.TabIndex = 3;
+            this.btnRecord.Text = "Record";
+            this.btnRecord.UseVisualStyleBackColor = true;
+            this.btnRecord.Click += new System.EventHandler(this.btnRecord_Click);
+            // 
+            // groupMode
+            // 
+            this.groupMode.Controls.Add(this.btnAnalogClock);
+            this.groupMode.Controls.Add(this.btnStopWatch);
+            this.groupMode.Location = new System.Drawing.Point(12, 104);
+            this.groupMode.Name = "groupMode";
+            this.groupMode.Size = new System.Drawing.Size(441, 67);
+            this.groupMode.TabIndex = 1;
+            this.groupMode.TabStop = false;
+            this.groupMode.Text = "Mode";
+            // 
+            // btnAnalogClock
+            // 
+            this.btnAnalogClock.AutoSize = true;
+            this.btnAnalogClock.Checked = true;
+            this.btnAnalogClock.Location = new System.Drawing.Point(31, 30);
+            this.btnAnalogClock.Name = "btnAnalogClock";
+            this.btnAnalogClock.Size = new System.Drawing.Size(127, 24);
+            this.btnAnalogClock.TabIndex = 0;
+            this.btnAnalogClock.TabStop = true;
+            this.btnAnalogClock.Text = "Analog Clock";
+            this.btnAnalogClock.UseVisualStyleBackColor = true;
+            this.btnAnalogClock.CheckedChanged += new System.EventHandler(this.btnAnalogClock_CheckedChanged);
+            // 
+            // btnStopWatch
+            // 
+            this.btnStopWatch.AutoSize = true;
+            this.btnStopWatch.Location = new System.Drawing.Point(209, 30);
+            this.btnStopWatch.Name = "btnStopWatch";
+            this.btnStopWatch.Size = new System.Drawing.Size(110, 24);
+            this.btnStopWatch.TabIndex = 1;
+            this.btnStopWatch.Text = "Stopwatch";
+            this.btnStopWatch.UseVisualStyleBackColor = true;
+            this.btnStopWatch.CheckedChanged += new System.EventHandler(this.btnStopWatch_CheckedChanged);
+            // 
+            // timerClock
+            // 
+            this.timerClock.Interval = 1000;
+            this.timerClock.Tick += new System.EventHandler(this.timerClock_Tick);
+            // 
+            // timerStopWatch
+            // 
+            this.timerStopWatch.Interval = 1;
+            this.timerStopWatch.Tick += new System.EventHandler(this.timerStopWatch_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1166, 863);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Illumina Analog Clock";
+            this.TransparencyKey = System.Drawing.SystemColors.Control;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupTimeDisplay.ResumeLayout(false);
+            this.groupStopWatch.ResumeLayout(false);
+            this.groupMode.ResumeLayout(false);
+            this.groupMode.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton btnStopWatch;
+        private System.Windows.Forms.RadioButton btnAnalogClock;
+        private System.Windows.Forms.GroupBox groupMode;
+        private System.Windows.Forms.Button btnRecord;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.GroupBox groupStopWatch;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Timer timerClock;
+        private System.Windows.Forms.Timer timerStopWatch;
+        private System.Windows.Forms.Label lblStopWatch;
+        private System.Windows.Forms.GroupBox groupTimeDisplay;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,395 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace AnalogClock
+{
+    public struct ClockArm
+    {
+        public PointF ptStart;
+        public PointF ptEnd;
+        public int nStart;
+        public Pen pen;
+        public Pen eraser;
+        public int nValue;
+        public float fArmLength;
+        public PointF GetEndPoint()
+        {
+            return new PointF(ptStart.X, ptStart.Y - fArmLength);
+        }
+    };
+    public partial class Form1 : Form
+    {
+        private PointF ptAnchor;
+        private PointF ptAnchorStopWatch;
+        private Graphics graph;
+        private ClockArm hourArm, minuteArm, secondArm, milSecArm;
+        private Pen penStopWatch;
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetupClockArm();
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timerClock.Enabled = true;
+        }
+
+        //setup clock arm
+        private void SetupClockArm()
+        {
+            graph = Graphics.FromHwnd(this.Handle);
+
+            ptAnchor = new PointF(this.Width / 2.0F, this.Height / 4.0F);
+            ptAnchorStopWatch = new PointF(ptAnchor.X, ptAnchor.Y - 30);
+
+            //draw arm for milSec
+            milSecArm.nStart = 0;
+            milSecArm.eraser = new Pen(Brushes.LightGray, 1.0f);
+            milSecArm.pen = new Pen(Brushes.Black, 1.0f);
+
+            //draw arm for second
+            secondArm.pen = new Pen(Brushes.DimGray, 2.0f);
+            secondArm.eraser = new Pen(Brushes.LightGray, 2.0f);
+            secondArm.ptStart = secondArm.ptEnd = ptAnchor;
+            secondArm.fArmLength = 100;
+
+            //draw arm for minute
+            minuteArm.eraser = new Pen(Brushes.LightGray, 3.0f);
+            minuteArm.pen = new Pen(Brushes.Orange, 3.0f);
+            minuteArm.ptStart = minuteArm.ptEnd = ptAnchor;
+            minuteArm.fArmLength = 80;
+
+            //draw arm for hour
+            hourArm.eraser = new Pen(Brushes.LightGray, 5.0f);
+            hourArm.pen = new Pen(Brushes.Teal, 5.0f);
+            hourArm.ptStart = hourArm.ptEnd = ptAnchor;
+            hourArm.fArmLength = 50;
+            //threadClock = new Thread(UpdateClock);
+
+            AdjustClockArmWithCurrentTime();
+            penStopWatch = new Pen(Brushes.Red, 3.0F);
+        }
+
+        //align clock arm with the current time
+        private void AdjustClockArmWithCurrentTime()
+        {
+            if (DateTime.Now.Hour == 0)
+            {
+                hourArm.nStart = hourArm.nValue = 0;
+
+            }
+            else if (DateTime.Now.Hour > 12)
+            {
+                hourArm.nStart = (DateTime.Now.Hour - 12) * 30;
+                hourArm.nValue = DateTime.Now.Hour - 12;
+            }
+            else if (DateTime.Now.Hour < 12)
+            {
+                hourArm.nStart = DateTime.Now.Hour * 30;
+                hourArm.nValue = DateTime.Now.Hour;
+            }
+
+            minuteArm.nStart = DateTime.Now.Minute * 6;
+            minuteArm.nValue = DateTime.Now.Minute;
+            secondArm.nStart = DateTime.Now.Second * 6;
+            secondArm.nValue = DateTime.Now.Second - 1;
+        }
+
+        //create watch face
+        private void CreateFaceWatch(PaintEventArgs e)
+        {
+            e.Graphics.FillEllipse(Brushes.LightGray, new RectangleF(ptAnchor.X - (250 / 2), ptAnchor.Y - (250 / 2), 250, 250));
+            e.Graphics.DrawEllipse(new Pen(Brushes.Gray, 10.0F), new RectangleF(ptAnchor.X - (250 / 2), ptAnchor.Y - (250 / 2), 250, 250));
+
+            PointF ptTarget = new PointF(ptAnchor.X, ptAnchor.Y - 100);
+            int _hour = 0;
+            for (int i = 0; i < 360; i += 6)
+            {
+                PointF ptTrajectory = GetTrajectoryPoint(ptAnchor, ptTarget, i);
+
+                PointF ptTarget2 = new PointF(ptTrajectory.X, ptTrajectory.Y - 10);
+                PointF ptTrajectory2 = GetTrajectoryPoint(ptTrajectory, ptTarget2, i);
+
+                PointF ptTarget3 = new PointF(ptTrajectory2.X, ptTrajectory2.Y - 15);
+                PointF ptTrajectory3 = GetTrajectoryPoint(ptTrajectory2, ptTarget3, i);
+
+                PointF ptTarget4 = new PointF(ptTrajectory2.X, ptTrajectory2.Y - 5);
+                PointF ptTrajectory4 = GetTrajectoryPoint(ptTrajectory2, ptTarget4, i);
+
+                if (i % 30 == 0)
+                {
+                    e.Graphics.DrawLine(new Pen(Brushes.Orange, 1.0f), ptTrajectory2, ptTrajectory3);
+                    if (_hour == 0)
+                    {
+                        e.Graphics.DrawString(12.ToString(), this.Font, Brushes.Navy, new PointF(ptTrajectory4.X - this.Font.SizeInPoints / 2, ptTrajectory4.Y - this.Font.SizeInPoints / 2));
+                    }
+                    else
+                    {
+                        e.Graphics.DrawString(_hour.ToString(), this.Font, Brushes.Navy, new PointF(ptTrajectory4.X - this.Font.SizeInPoints / 2, ptTrajectory4.Y - this.Font.SizeInPoints / 2));
+                    }
+                    _hour++;
+
+                }
+                else
+                {
+                    e.Graphics.DrawLine(new Pen(Brushes.Teal, 1.0f), ptTrajectory2, ptTrajectory3);
+                }
+            }
+        }
+
+        private void btnStopWatch_CheckedChanged(object sender, EventArgs e)
+        {
+            if (btnStopWatch.Checked)
+            {
+                hourArm.nStart = hourArm.nValue = 0;
+                minuteArm.nStart = minuteArm.nValue = 0;
+                secondArm.nStart = secondArm.nValue = 0;
+                milSecArm.nStart = milSecArm.nValue = 0;
+                lblStopWatch.Text = GetStopWatchLabel(hourArm.nValue, minuteArm.nValue, secondArm.nValue, milSecArm.nValue, true);
+                RefreshClock(true);
+                timerClock.Enabled = false;
+                groupStopWatch.Enabled = true;
+
+            }
+        }
+
+        private void btnAnalogClock_CheckedChanged(object sender, EventArgs e)
+        {
+            if (btnAnalogClock.Checked)
+            {
+                AdjustClockArmWithCurrentTime();
+                timerClock.Enabled = true;
+
+                groupStopWatch.Enabled = false;
+
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            //face watch
+            CreateFaceWatch(e);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timerClock.Start();
+            timerStopWatch.Start();
+            btnStart.Enabled = false;
+        }
+
+        private void btnRecord_Click(object sender, EventArgs e)
+        {
+            listBox.Items.Add(GetStopWatchLabel(hourArm.nValue, minuteArm.nValue, secondArm.nValue, milSecArm.nValue, true));
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ResetStopWatch();
+            RefreshClock(true);
+            listBox.Items.Clear();
+        }
+
+        private void RefreshClock(bool bForStopWatch)
+        {
+            graph.DrawLine(secondArm.eraser, secondArm.ptStart, secondArm.ptEnd);
+            graph.DrawLine(minuteArm.eraser, minuteArm.ptStart, minuteArm.ptEnd);
+            graph.DrawLine(hourArm.eraser, hourArm.ptStart, hourArm.ptEnd);
+
+            PointF ptSecondEndPoint = GetTrajectoryPoint(secondArm.ptStart, secondArm.GetEndPoint(), secondArm.nStart);
+            PointF ptMinuteEndPoint = GetTrajectoryPoint(minuteArm.ptStart, minuteArm.GetEndPoint(), minuteArm.nStart);
+            PointF ptHourEndPoint = GetTrajectoryPoint(hourArm.ptStart, hourArm.GetEndPoint(), hourArm.nStart);
+
+            graph.DrawEllipse(secondArm.eraser, secondArm.ptEnd.X - 2.5F, secondArm.ptEnd.Y - 2.5F, 5, 5);
+
+            secondArm.ptEnd = ptSecondEndPoint;
+            minuteArm.ptEnd = ptMinuteEndPoint;
+            hourArm.ptEnd = ptHourEndPoint;
+            graph.DrawLine(secondArm.pen, secondArm.ptStart, ptSecondEndPoint);
+            graph.DrawLine(minuteArm.pen, minuteArm.ptStart, ptMinuteEndPoint);
+            graph.DrawLine(hourArm.pen, hourArm.ptStart, ptHourEndPoint);
+
+            graph.DrawEllipse(secondArm.pen, ptSecondEndPoint.X - 2.5F, ptSecondEndPoint.Y - 2.5F, 5, 5);
+
+            //arm movement
+            secondArm.nStart += 6;
+            if (secondArm.nStart >= 360)
+            {
+                secondArm.nStart = 0;
+                minuteArm.nStart += 6;
+                if (minuteArm.nStart >= 360)
+                {
+                    minuteArm.nStart = 0;
+                    hourArm.nStart += 30;
+                    if (hourArm.nStart >= 360)
+                    {
+                        hourArm.nStart = 0;
+                    }
+                }
+            }
+
+            //track timing
+            if (!bForStopWatch)
+            {
+                secondArm.nValue++;
+            }
+            if (secondArm.nValue > 60)
+            {
+                secondArm.nValue = 0;
+                minuteArm.nValue++;
+                if (minuteArm.nValue > 60)
+                {
+                    minuteArm.nValue = 0;
+                    hourArm.nValue++;
+                    if (hourArm.nValue > 60)
+                    {
+                        hourArm.nValue++;
+                        if (hourArm.nValue > 12)
+                        {
+                            hourArm.nValue = 0;
+                        }
+                    }
+                }
+
+            }
+            if (!bForStopWatch)
+            {
+                lblStopWatch.Text = GetStopWatchLabel(hourArm.nValue, minuteArm.nValue, secondArm.nValue, milSecArm.nValue, false);
+            }
+        }
+
+        private void timerStopWatch_Tick(object sender, EventArgs e)
+        {
+            RunStopWatch();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void timerClock_Tick(object sender, EventArgs e)
+        {
+            RefreshClock(false);
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timerStopWatch.Enabled = false;
+            timerClock.Enabled = false;
+            btnStart.Enabled = true;
+        }
+
+        private string GetStopWatchLabel(int hour, int minute, int second, int milsec, bool bStopWatch)
+        {
+
+            string _hour, _minute, _second, _milsec;
+            if (hour < 10)
+            {
+                _hour = "0" + hour;
+            }
+            else
+            {
+                _hour = hour.ToString();
+            }
+
+            if (minute < 10)
+            {
+                _minute = "0" + minute;
+            }
+            else
+            {
+                _minute = minute.ToString();
+            }
+            if (second < 10)
+            {
+                _second = "0" + second;
+            }
+            else
+            {
+                _second = second.ToString();
+            }
+
+            if (milsec < 10)
+            {
+                _milsec = "0" + milsec;
+            }
+            else
+            {
+                _milsec = milsec.ToString();
+            }
+
+            if (bStopWatch)
+            {
+                return _hour + ":" + _minute + ":" + _second + ":" + _milsec;
+            }
+            else
+            {
+                return _hour + ":" + _minute + ":" + _second;
+            }
+        }
+
+        private void ResetStopWatch()
+        {
+            hourArm.nStart = hourArm.nValue = 0;
+            minuteArm.nStart = minuteArm.nValue = 0;
+            secondArm.nStart = secondArm.nValue = 0;
+            milSecArm.nStart = milSecArm.nValue = 0;
+            CreateGraphics().DrawArc(new Pen(Brushes.Gray, 10.0F), new RectangleF(ptAnchor.X - (250 / 2), ptAnchor.Y - (250 / 2), 250, 250), -90.0F, 360.0F);
+            lblStopWatch.Text = GetStopWatchLabel(hourArm.nValue, minuteArm.nValue, secondArm.nValue, milSecArm.nValue, true);
+        }
+
+        private void RunStopWatch()
+        {
+            PointF ptMilSecEndPoint = GetTrajectoryPoint(milSecArm.ptStart, milSecArm.GetEndPoint(), milSecArm.nStart);
+            milSecArm.ptEnd = ptMilSecEndPoint;
+
+            milSecArm.nStart += 6;
+            milSecArm.nValue++;
+            CreateGraphics().DrawArc(penStopWatch, new RectangleF(ptAnchor.X - (250 / 2), ptAnchor.Y - (250 / 2), 250, 250), -90.0F, milSecArm.nStart);
+
+
+            if (milSecArm.nStart >= 360)
+            {
+                milSecArm.nStart = 0;
+                if (secondArm.nValue % 2 == 0)
+                {
+                    penStopWatch.Brush = Brushes.Gray;
+                }
+                else
+                {
+                    penStopWatch.Brush = Brushes.Red;
+                }
+            }
+            if (milSecArm.nValue > 60)
+            {
+                milSecArm.nValue = 0;
+            }
+            lblStopWatch.Text = GetStopWatchLabel(hourArm.nValue, minuteArm.nValue, secondArm.nValue, milSecArm.nValue, true);
+        }
+
+        //return trajectory point based on anchorPoint
+        public PointF GetTrajectoryPoint(PointF anchorPoint, PointF targetPoint, double dRotation)
+        {
+            PointF ptNew = new PointF();
+            PointF ptShift = new PointF(targetPoint.X - anchorPoint.X, targetPoint.Y - anchorPoint.Y);
+            ptNew.X = anchorPoint.X - (ptShift.X * ((float)Math.Cos(dRotation * Math.PI / 180.0)) + ptShift.Y * ((float)Math.Sin(dRotation * Math.PI / 180.0)));
+            ptNew.Y = anchorPoint.Y - (ptShift.X * (float)Math.Sin(dRotation * Math.PI / 180.0) - ptShift.Y * (float)Math.Cos(dRotation * Math.PI / 180.0));
+            return ptNew;
+        }
+    }
+
+
+}
